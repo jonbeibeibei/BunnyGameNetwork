b435e8b31b02bd1fbfaf37c9ed385c5f9a67ab26
Jonathan Bei Qi Yang <jonathanbeiqiyang@Jonathans-MacBook-Pro.local>
2017-03-23 14:24:16 +0800

    First Commit

---
+++ assets/scripts/cameraCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace UnityStandardAssets.Cameras
+{
+
+	public class cameraCheck : NetworkBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			if (isLocalPlayer) {
+//				GetComponentInChildren<AutoCam>().enabled = true;
+				print ("true");
+			}
+			print ("false");
+		}
+
+
+
+	}
+}
---
+++ assets/scripts/localCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+namespace UnityStandardAssets.Characters.ThirdPerson{
+
+
+public class localCheck : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+			
+			if (isLocalPlayer) {
+
+				GetComponentInChildren<ThirdPersonCharacter> ().enabled = true;
+				GetComponentInChildren<ThirdPersonUserControl> ().enabled = true;
+			}
+			print ("False");
+		}
+	
+	}
+}
